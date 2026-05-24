e40afb4409b9083ceab2ae6509559af1dc516a4e
kuslhhh <kushaljadhav7745@gmail.com>
2026-05-24 12:47:41 +0530

    setup dockerized infrastructure with nginx and prisma migrations

--- server/prisma/migrations/20260523192723_init/migration.sql
+++ server/prisma/migrations/20260523192723_init/migration.sql
@@ -1,13 +1,14 @@
 -- CreateTable
-CREATE TABLE "Url" (
-    "id" TEXT NOT NULL,
+CREATE TABLE "Link" (
+    "id" SERIAL NOT NULL,
     "originalUrl" TEXT NOT NULL,
     "shortCode" TEXT NOT NULL,
     "clicks" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Url_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Url_shortCode_key" ON "Url"("shortCode");
+CREATE UNIQUE INDEX "Link_shortCode_key" ON "Link"("shortCode");
